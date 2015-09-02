7510476084c79ce3ce2d3632e430c7f477b3b3d3
Sean Wood (AWSWi7) <sean@anotherway.co.za>
2015-09-02 14:51:10 +0200

    Completed connections page, fixed up multiple handlers not being removed

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,58 @@ angular.module('tcpTestApp', ['ionic', 'tcpTestApp.controllers'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('app.connections', {
+    url: '/connections',
+    views: {
+      'tab-connections': {
+        templateUrl: 'templates/tab-connections.html',
+        // The controller is not needed since 'tcpController' is not specific to this view and is already applied in index.html
+        // controller: 'tcpController'
+      }
+    }
+  })
+
+  .state('app.control', {
+    url: '/control',
+    views: {
+      'tab-control': {
+        templateUrl: 'templates/tab-control.html',
+        // The controller is not needed since 'tcpController' is not specific to this view and is already applied in index.html
+        // controller: 'tcpController'
+      }
+    }
+  })
+
+  .state('app.telemetry', {
+    url: '/telemetry',
+    views: {
+      'tab-telemetry': {
+        templateUrl: 'templates/tab-telemetry.html',
+        // The controller is not needed since 'tcpController' is not specific to this view and is already applied in index.html
+        // controller: 'tcpController'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/connections');
+
+});
